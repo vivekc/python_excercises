d9fc362c05223f7a7cd07512434f6ba22584b822
unknown <vchaure@MUMD0761.corp.capgemini.com>
2017-12-11 17:08:36 +0530

    memoization using decorators

--- rotate.py
+++ rotate.py
@@ -1,6 +1,23 @@
-def leftRotation(l, d):
-    # left rotate array l by d positions
-    # e.g. if a = [5, 1, 2, 3, 1] and d = 4
+from functools import wraps
+
+
+def memoize(func):
+    """momoize func's return value in a dictionary"""
+    cache = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        key = (tuple(args[0]), args[1])
+        if key not in cache:
+            cache[key] = func(*args)
+        return cache[key]
+    return wrapper
+
+
+@memoize
+def left_rotation(l, d, **kwargs):
+    """left rotate array l by d positions
+    e.g. if a = [5, 1, 2, 3, 1] and d = 4"""
     for i in range(d):
         temp = l[i]
         for j in range(1, len(l)):
@@ -9,6 +26,14 @@ def leftRotation(l, d):
     return l
 
 
-a = [5, 1, 2, 3, 1]
-print a
-print(leftRotation(a, 3))
+if __name__ == "__main__":
+    a = [
+        [5, 1, 2, 3, 1, 5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1],
+        [5, 1, 2, 3, 1, 5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1],
+        [5, 1, 2, 3, 1, 5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1],
+        [5, 1, 2, 3, 1, 5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1],
+        [5, 1, 2, 3, 1, 5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1,5, 1, 2, 3, 1]
+        ]
+    for aa in a:
+        print(left_rotation(aa, len(aa)))
+
